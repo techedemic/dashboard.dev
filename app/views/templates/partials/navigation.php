1cf3fd3ec85fa003c24779ce78f6329a7a0274b4
Techedemic <hendri@techedemic.com>
2016-11-30 14:21:42 +0200

    Before video 26

--- app/views/templates/partials/navigation.php
+++ app/views/templates/partials/navigation.php
@@ -6,8 +6,10 @@
     <li><a href="{{ urlFor('home') }}">Home</a></li>
     {% if auth %}
     <li><a href="{{ urlFor('logout') }}">Logout</a></li>
+    <li><a href="{{ urlFor('user.profile', {username:auth.username})}}">Your profile</a></li>
     {% else %}
     <li><a href="{{ urlFor('register') }}">Register</a></li>
     <li><a href="{{ urlFor('login') }}">Login</a></li>
     {% endif %}
+    <li><a href="{{ urlFor('user.all') }}">All Users</a></li>
 </ul>
